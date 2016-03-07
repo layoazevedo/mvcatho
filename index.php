4fdce5110224a68db80107b359aba7ec21ca6686
Layo Azevedo <layoazevedo@gmail.com>
2016-03-06 21:36:11 -0300

    add comentario

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ ini_set('display_errors',1);
 ini_set('display_startup_erros',1);
 error_reporting(E_ALL);
 
+// inicia PSR
 require "vendor/autoload.php";
 
 $application = new MvCatho\System\CathoCore();
